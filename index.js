4dcd7545f23fe4f4a1629f9c0eab372cc703c700
Florian Lutz <florian.lutz@ymail.com>
2024-02-28 14:40:55 +0100

    initial commit, add tasks

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// 1, Corina - API: fetch characters from show - fetchCharacters function, do import
+// 2, Florian - create function to create cards from API Data (createCharacterCard in cardContainer.js)
+//     -parameters: src, name, status, type, occurences - li as containter returned
+// 3, Corina - pagination: several pages with 20 char each
+// 4, Florian - searchbar: searchQuery
+// 5, BONUS: refactor
+// 6, styling
+// 7, tests
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
