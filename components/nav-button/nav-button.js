c7e3ffcbc0033a38e8917584c3ba0ba98ae9d9f5
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-01 13:52:30 +0100

    refactoring

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -1,30 +1,13 @@
-import { nextButton, prevButton, fetchData } from "/index.js";
+import { nextButton, prevButton, fetchData } from "../../index.js";
 
 export function nextButtonEvent() {
   nextButton.addEventListener("click", function (event) {
-    //   cardContainer.innerHTML = "";
-    //   pageNumber++;
-    //   const url = `${APIurl}page=${pageNumber}`;
     fetchData("up");
-    // pagination();
-    // pagination.textContent = `${pageNumber}/${maxPage}`;
-    // prevButton.classList.remove("hidden");
-    // if (pageNumber === maxPage) {
-    //   nextButton.classList.add("hidden");
-    // }
   });
 }
 
 export function prevButtonEvent() {
   prevButton.addEventListener("click", function (event) {
-    //   cardContainer.innerHTML = "";
-    //   pageNumber--;
-    //   const url = `https://rickandmortyapi.com/api/character?page=${pageNumber}`;
     fetchData("down");
-    // pagination();
-    // pagination.textContent = `${pageNumber}/${maxPage}`;
-    // nextButton.classList.remove("hidden");
-    // if (pageNumber === 1) {
-    //   prevButton.classList.add("hidden");
   });
 }
